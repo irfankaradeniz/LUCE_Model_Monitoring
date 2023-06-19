347c4a5c9e60493f49cdee3f8629e7786bcc1f22
Irfan Karadeniz <ikrdnz94@gmail.com>
2023-06-19 14:49:20 +0200

    redesign code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import logging
 
 from sklearn.model_selection import KFold
@@ -10,51 +9,55 @@ from metadata import generate_metadata, calculate_gower_similarity
 from model_training import train_test_split_with_encoding, get_performance_metrics_on_synthetic_datasets, train_evaluate_and_validate_models
 from visualisations import visualize_gower_similarity, plot_performance_metrics
 
+# Constants
+DATASET_PATH = "/data/heart.csv"
+METADATA_SCHEMA_PATH = "/metadata/metadata_schema.json"
+TARGET_VARIABLE = "HeartDisease"
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+N_SYNTHETIC_DATASETS = 10
+N_SPLITS = 5  # for k-fold cross-validation
+
 # Set up logging configurations
 LOGGING_CONFIG = {"filename": "simulation.log", "level": logging.INFO, "format": "%(asctime)s %(message)s"}
 logging.basicConfig(**LOGGING_CONFIG)
 
 def main():
-    # Define constants
-    DATASET_PATH = "/Users/irfankaradeniz/Downloads/heart.csv"
-    METADATA_SCHEMA_PATH = "metadata_schema.json"
-    TARGET_VARIABLE = "HeartDisease"
-    TEST_SIZE = 0.2
-    RANDOM_STATE = 42
-    N_SYNTHETIC_DATASETS = 10
-    N_SPLITS = 5  # for k-fold cross-validation
-
-    # Step 1: Load the dataset
+    """
+    Main function to execute the data simulation, model training, evaluation, and validation process.
+    """
+    # Load the dataset
     loaded_dataset = load_dataset(DATASET_PATH)
 
-    # Step 2: Generate metadata for the loaded dataset
+    # Generate metadata for the loaded dataset
     metadata_schema = pd.read_json(METADATA_SCHEMA_PATH)
     loaded_dataset_metadata = generate_metadata(loaded_dataset, metadata_schema)
 
-    # Step 3: Generate synthetic datasets
+    # Generate synthetic datasets
     synthetic_datasets = generate_synthetic_datasets(loaded_dataset, TARGET_VARIABLE, n=N_SYNTHETIC_DATASETS)
 
-    # Step 4: Generate metadata for synthetic datasets
+    # Generate metadata for synthetic datasets
     synthetic_datasets_metadata_list = [generate_metadata(df, metadata_schema.copy()) for df in synthetic_datasets]
 
-    # Step 5: Find the most similar dataset using Gower's similarity
+    # Find the most similar dataset using Gower's similarity
     gower_similarity_scores = calculate_gower_similarity(loaded_dataset_metadata, synthetic_datasets_metadata_list)
     most_similar_dataset = synthetic_datasets[np.argmax(gower_similarity_scores)]
 
     # Visualize Gower's similarity results
     visualize_gower_similarity(loaded_dataset, synthetic_datasets, gower_similarity_scores)
 
-    # Step 6: Train and evaluate models using k-fold cross-validation and validate them
+    # Define classifiers and k-fold for model training
     classifiers = [{"name": "Random Forest", "clf": RandomForestClassifier(random_state=RANDOM_STATE)}]
     kfold = KFold(n_splits=N_SPLITS, random_state=RANDOM_STATE, shuffle=True)
 
-    X_train, X_test, y_train, y_test = train_test_split_with_encoding(loaded_dataset, TARGET_VARIABLE, test_size=TEST_SIZE, random_state=RANDOM_STATE)
-    train_evaluate_and_validate_models(classifiers, kfold, X_train, y_train, most_similar_dataset, TARGET_VARIABLE)
+    # Train, evaluate, and validate models
+    training_data, test_data, training_targets, test_targets = train_test_split_with_encoding(loaded_dataset, TARGET_VARIABLE, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+    train_evaluate_and_validate_models(classifiers, kfold, training_data, training_targets, most_similar_dataset, TARGET_VARIABLE)
 
-    # Step 7: Train and evaluate the model on each synthetic dataset and store the metrics
+    # Train and evaluate the model on each synthetic dataset and store the metrics
     performance_metrics_list = get_performance_metrics_on_synthetic_datasets(synthetic_datasets, classifiers, TARGET_VARIABLE, TEST_SIZE, RANDOM_STATE)
 
-    # Step 8: Plot the performance metrics across synthetic datasets
+    # Plot the performance metrics across synthetic datasets
     metric_names = ["accuracy", "recall", "f1", "precision", "roc_auc"]
     plot_performance_metrics(performance_metrics_list, metric_names)
 
